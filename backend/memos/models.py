948001647a14987cfc156d625ad97f2dd98259b3
dwnusa <dwnusa@JLK1ui-iMac-Pro-2.local>
2019-08-20 18:42:50 +0900

    update

--- backend/memos/models.py
+++ backend/memos/models.py
@@ -4,13 +4,19 @@ from django.db import models
 
 from django.db import models
 
-class Memo(models.Model):
-    # userId = models.IntegerField(blank=True, null=True)
-    userId = models.CharField(max_length=50, default='anonymous')
-    title = models.CharField(max_length=50)
-    content = models.TextField()
+
+class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
+
+class Memo(TimeStampedModel):
+    userId = models.CharField(max_length=50, default='anonymous')
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+
     def __str__(self):
-        return self.title
+        return self.title + "  -  " + self.content
