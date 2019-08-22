91251417a77418b1702c19cd187ced8e3fa9553d
dwnusa <dwnusa@JLK1ui-iMac-Pro-2.local>
2019-08-22 18:13:06 +0900

    updated view for commit and like

--- backend/memos/urls.py
+++ backend/memos/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('all/', views.MemoList.as_view(), name='MemoList'),
     # path('<int:memo_id>/', views.MemoDetail.as_view(), name='MemoDetail'),
     path('<int:memo_id>/comments/', views.CommentList.as_view(), name='CommentList'),
+    path('<int:memo_id>/comments/<int:comment_id>', views.CommentDelete.as_view(), name='CommentDelete'),
     path('<int:memo_id>/likes/', views.LikeList.as_view(), name='LikeList'),
-    path('<int:memo_id>/like/', views.LikeMemo.as_view(), name='LikeMemo'),
-    path('<int:memo_id>/unlike/', views.UnlikeMemo.as_view(), name='UnlikeMemo'),
+    # path('<int:memo_id>/like/', views.LikeMemo.as_view(), name='LikeMemo'),
+    # path('<int:memo_id>/unlike/', views.UnlikeMemo.as_view(), name='UnlikeMemo'),
     # path('<int:pk>/', views.MemoDetail.as_view(), name='MemoDetail'),
     # path('create/', views.MemoCreate.as_view()),
 ]
--- backend/memos/views.py
+++ backend/memos/views.py
@@ -32,45 +32,110 @@ class CommentList(APIView):
 
         return Response(data=serializer.data)
 
+    def post(self, request, memo_id, format=None):
+        msg = request.data['message']
+        print('Memo_id: ' + str(memo_id) + ' , Comment: ' + str(msg))
+        try:
+            found_memo = models.Memo.objects.get(id=memo_id)
+        except models.Memo.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        new_comment = models.Comment.objects.create(
+            message=msg,
+            memo=found_memo
+        )
+        new_comment.save()
+        all_comments = models.Comment.objects.filter(memo=memo_id)
+        serializer = serializers.CommentSerializer(all_comments, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class CommentDelete(APIView):
+    def get(self, request, memo_id, comment_id, format=None):
+        print('get comments: ' + str(comment_id) + ' from memo id: '+ str(memo_id))
+        target_comment = models.Comment.objects.filter(memo=memo_id, id=comment_id)
+        if not target_comment:
+            print('empty')
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = serializers.CommentSerializer(target_comment, many=True)
+        return Response(data=serializer.data,  status=status.HTTP_200_OK)
+
+    def delete(self, request, memo_id, comment_id, format=None):
+        print('get comments: ' + str(comment_id) + ' from memo id: '+ str(memo_id))
+        target_comment = models.Comment.objects.filter(memo=memo_id, id=comment_id)
+        if not target_comment:
+            print('empty')
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        target_comment.delete()
+        all_comments = models.Comment.objects.filter(memo=memo_id)
+        serializer = serializers.CommentSerializer(all_comments, many=True)
+        return Response(status=status.HTTP_202_ACCEPTED, data=serializer.data)
 
 class LikeList(APIView):
     def get(self, request, memo_id, format=None):
         print(memo_id)
-
         all_likes = models.Like.objects.filter(memo=memo_id)
-
         serializer = serializers.LikeSerializer(all_likes, many=True)
-
         return Response(data=serializer.data)
 
-
-class LikeMemo(APIView):
     def post(self, request, memo_id, format=None):
         print('Like:' + str(memo_id))
         try:
             found_memo = models.Memo.objects.get(id=memo_id)
-            # serializer = serializers.MemoSerializer(found_memo, many=True)
         except models.Memo.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-        # for element in found_memo:
-        # print(found_memo[0])
-        # print(found_memo)
-
         new_like = models.Like.objects.create(
             memo=found_memo
         )
         new_like.save()
+        all_likes = models.Like.objects.filter(memo=memo_id)
+        serializer = serializers.LikeSerializer(all_likes, many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
 
-        return Response(status=200)
-
-
-class UnlikeMemo(APIView):
-    def post(self, request, memo_id, format=None):
+    def delete(self, request, memo_id, format=None):
         print('Unlike:' + str(memo_id))
 
-        return Response(status=200)
+        try:
+            preexisting_like = models.Like.objects.filter(
+                memo=memo_id
+            ).order_by('id').first()
+            preexisting_like.delete()
+            all_likes = models.Like.objects.filter(memo=memo_id)
+            serializer = serializers.LikeSerializer(all_likes, many=True)
+            return Response(status=status.HTTP_202_ACCEPTED, data=serializer.data)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
+
+# class LikeMemo(APIView):
+#     def post(self, request, memo_id, format=None):
+#         print('Like:' + str(memo_id))
+#         try:
+#             found_memo = models.Memo.objects.get(id=memo_id)
+#         except models.Memo.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         new_like = models.Like.objects.create(
+#             memo=found_memo
+#         )
+#         new_like.save()
+#         return Response(status=status.HTTP_200_OK)
+#
+#
+# class UnlikeMemo(APIView):
+#     def delete(self, request, memo_id, format=None):
+#         print('Unlike:' + str(memo_id))
+#         try:
+#             found_memo = models.Memo.objects.get(id=memo_id)
+#         except models.Memo.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         try:
+#             preexisting_like = models.Like.objects.filter(
+#                 memo=found_memo
+#             ).order_by('id').first()
+#             preexisting_like.delete()
+#             return Response(status=status.HTTP_202_ACCEPTED)
+#         except:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 
