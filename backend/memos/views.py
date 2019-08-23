4f14f34e88edcc8bcdfbe09078ec1862499305a6
daewoon kim <dwnusa@gmail.com>
2019-08-23 14:56:12 +0900

    update for APIViews

--- backend/memos/views.py
+++ backend/memos/views.py
@@ -1,11 +1,3 @@
-# from django.shortcuts import render
-# from . import models
-
-# Create your views here.
-# posts/views.py
-# from rest_framework import generics
-# from .models import Memo
-# from .serializers import MemoSerializer
 from rest_framework.views import APIView
 from . import models, serializers
 from rest_framework.response import Response
@@ -14,34 +6,28 @@ from rest_framework import status
 
 class MemoList(APIView):
     def get(self, request, format=None):
-
         all_memos = models.Memo.objects.all()
-
         serializer = serializers.MemoSerializer(all_memos, many=True)
-
         return Response(data=serializer.data)
 
 
 class CommentList(APIView):
     def get(self, request, memo_id, format=None):
         print(memo_id)
-
         all_comments = models.Comment.objects.filter(memo=memo_id)
-
         serializer = serializers.CommentSerializer(all_comments, many=True)
-
-        return Response(data=serializer.data)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, memo_id, format=None):
         msg = request.data['message']
         print('Memo_id: ' + str(memo_id) + ' , Comment: ' + str(msg))
-        try:
-            found_memo = models.Memo.objects.get(id=memo_id)
-        except models.Memo.DoesNotExist:
+        found_memo = models.Memo.objects.filter(id=memo_id)
+        if not found_memo:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
         new_comment = models.Comment.objects.create(
             message=msg,
-            memo=found_memo
+            memo=found_memo[0]
         )
         new_comment.save()
         all_comments = models.Comment.objects.filter(memo=memo_id)
@@ -69,6 +55,7 @@ class CommentDelete(APIView):
         all_comments = models.Comment.objects.filter(memo=memo_id)
         serializer = serializers.CommentSerializer(all_comments, many=True)
         return Response(status=status.HTTP_202_ACCEPTED, data=serializer.data)
+
 
 class LikeList(APIView):
     def get(self, request, memo_id, format=None):
