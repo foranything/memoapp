50f3ea2429eb30a616e361d4a6fb015d871d983a
daewoon kim <dwnusa@gmail.com>
2019-09-02 10:23:54 +0900

    test code step2

--- frontend-tdd/react-enzyme-test/src/Profile.test.js
+++ frontend-tdd/react-enzyme-test/src/Profile.test.js
@@ -7,4 +7,10 @@ describe('<Profile />', ()=>{
     const wrapper = mount(<Profile username="velopert" name="김민준" />);
     expect(wrapper).toMatchSnapshot();
   });
+  
+  it('renders username and name', () => {
+    const wrapper = mount(<Profile username="velopert" name="김민준" />);
+    expect(wrapper.props().username).toBe('velopert');
+    expect(wrapper.props().name).toBe('김민준');
+  });
 });
