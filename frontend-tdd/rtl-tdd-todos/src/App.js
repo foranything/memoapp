4a97c5b042f20bde334531739c26b34116c98bf6
daewoon kim <dwnusa@gmail.com>
2019-09-03 13:17:34 +0900

    update

--- frontend-tdd/rtl-tdd-todos/src/App.js
+++ frontend-tdd/rtl-tdd-todos/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import TodoApp from './TodoApp'
 import TodoItem from './TodoItem'
 import DelayedToggle from './DelayedToggle';
-import UserProfile from './UserProfile'
+import ValidateSample from './ValidateSample'
 import './App.css';
 
 function App() {
   
   return (
     <div>
-      <UserProfile id={1}/>
+      <ValidateSample />
       {/*<DelayedToggle />*/}
       {/*<TodoApp/>*/}
     </div>
