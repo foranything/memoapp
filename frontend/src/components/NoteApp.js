35782480078ff8e0ecf7d3c559d910f28312dc91
dwnusa <dwnusa@gmail.com>
2019-09-06 21:42:30 +0900

    update memoapp front

--- frontend/src/components/NoteApp.js
+++ frontend/src/components/NoteApp.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NoteList from './NoteList'
 import NoteWriter from './NoteWriter'
 import * as service from "../services/noteAPI";
 
 class NoteApp extends Component {
-  state ={
+  state = {
     fetching: false,
-    notes : [
-      {id: 1, title: 'title1', content: 'content1', password:'password1'},
-      {id: 2, title: 'title2', content: 'content2', password:'password2'},
-      {id: 3, title: 'title3', content: 'content3', password:'password3'},
+    notes: [
+      { id: 1, title: 'title1', content: 'content1', password: 'password1' },
+      { id: 2, title: 'title2', content: 'content2', password: 'password2' },
+      { id: 3, title: 'title3', content: 'content3', password: 'password3' },
     ]
   };
-  
+
   componentDidMount() {
     this.fetchNotes();
   }
-  
+
   fetchNotes = async postId => {
     const { notes } = this.state;
     this.setState({
@@ -24,22 +24,19 @@ class NoteApp extends Component {
     });
     try {
       const info = await Promise.all([service.getNote()]);
-      const receivedNotes = info[0].data;
-      receivedNotes.reverse();
-      // console.log(receivedNotes);
-      // console.log(notes);
-      if (receivedNotes.length < 4){
-        receivedNotes.map((note, index) => {
-          notes[index].id=index;
-          notes[index].title=note.title;
-          notes[index].content=note.content;
-          notes[index].password=note.password;
+      const savedNotes = info[0].data;
+      if (savedNotes.length <= 3) {
+        notes.map((note, index) => {
+          notes[index].id = index;
+          notes[index].title = savedNotes[index].title;
+          notes[index].content = savedNotes[index].content;
+          notes[index].password = savedNotes[index].password;
         });
       }
       else {
         this.setState({
           fetching: false, // done!
-          notes:receivedNotes,
+          notes: savedNotes,
         });
       }
       this.setState({
@@ -55,52 +52,20 @@ class NoteApp extends Component {
       });
     }
   };
-  
+
   save = async newNote => {
-    const { notes } = this.state;
-    this.setState({
-      fetching: true // requesting..
-    });
-    try {
-      const info = await Promise.all([service.postNote(newNote)]);
-      // console.log(info[0].data[0]);
-      const postedNotes = info[0].data;
-      postedNotes.reverse();
-      // if (receivedNotes.length < 4){
-      //   receivedNotes.map((receivedNote, index) => notes[index]=receivedNote);
-      // }
-      // else {
-      //   this.setState({
-      //     fetching: false, // done!
-      //     // notes:receivedNotes,
-      //     notes:[
-      //       ...receivedNotes,
-      //     ]
-      //   });
-      // }
-      this.setState({
-        fetching: false, // done!
-        // notes: postedNotes,
-        notes:[
-          ...notes,
-          ...postedNotes,
-        ]
-      });
-    } catch (e) {
-      // if err, stop at this point
-      this.setState({
-        fetching: false
-      });
-    }
+    // const { notes } = this.state;
+    await Promise.all([service.postNote(newNote)]);
+    this.fetchNotes();
   };
-  
+
   render() {
-    const {notes} = this.state;
+    const { notes } = this.state;
     // console.log(notes);
     return (
       <div>
-        <NoteWriter save={this.save}/>
-        <NoteList   notes ={notes}/>
+        <NoteWriter save={this.save} />
+        <NoteList notes={notes} />
       </div>
     );
   }
--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Note from './Note'
 
 class NoteList extends Component {
   render() {
-    const {notes} = this.props;
+    const { notes } = this.props;
     return (
       <div className='NoteList'>
-        {notes.reverse().map((note, index) =>
-          <Note username={note.username} title={note.title} content={note.content} key={index}/>)}
+        {notes.map((note, index) =>
+          <Note username={note.username} title={note.title} content={note.content} key={index} />)}
       </div>
     );
   }
